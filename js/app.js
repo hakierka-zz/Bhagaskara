724bc53c5220888b1990419e537c02882e4e6194
Amanda Waliszewska <amy.wal@hotmail.com>
2015-12-16 11:41:34 +0100

    Skill section almost complete

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@
  * Created by Hakierka on 10.12.2015.
  */
 $(document).ready(function(){
-
+    //Sticky menu
     var menu=$("nav");
     var menuOffsetOfTop = menu.position().top;
-    $(window).on("scroll", function(event){
+    $(window).on("scroll", function(){
         if ($(window).scrollTop()> menuOffsetOfTop) {
             menu.addClass("sticky");
         }
@@ -15,7 +15,14 @@ $(document).ready(function(){
 
 
     });
+    //End sticky menu
 
+    //Skill bars
+        $('.skillbar').each(function(){
+            $(this).find('.skillbar-bar').animate({
+                width:$(this).attr('data-percent')
+            },6000);
+        });
 
-
+    //End Skill bars
 });
